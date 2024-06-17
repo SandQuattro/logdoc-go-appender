06d6a862af23a61389c16c170c0354279b5ad5f6
sandquattro <sandquattro@gmail.com>
2024-06-17 14:20:00 +0300

    v0.0.45

--- slog/slog_handler.go
+++ slog/slog_handler.go
@@ -165,7 +165,7 @@ func (h *LogdocHandler) sendLogdoc(level string, entry *slog.Record, err error) 
 	}
 
 	t := time.Now()
-	tsrc := t.Format("060102150405.00000") + "\n"
+	tsrc := t.Format("060102150405.000") + "\n"
 	tsrc = strings.ReplaceAll(tsrc, ".", "")
 
 	// Пишем заголовок
